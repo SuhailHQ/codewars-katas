5cdaa11fd7b6350b86ee07d8ce34affdd812e4ed
SuhailHQ <elbaqqalysouhayl@gmail.com>
2025-09-16 13:25:22 +0100

    keep_hydrated

---
+++ 8kyu/keep_hydrated/keep_hydrated.c
@@ -0,0 +1,11 @@
+int Liters(double time) 
+{
+  //The fun begins here.
+  double    liters;
+  int       littersCost;
+
+  liters = 0.5;
+  littersCost = 0;
+  littersCost = time * liters;
+  return littersCost;
+}
